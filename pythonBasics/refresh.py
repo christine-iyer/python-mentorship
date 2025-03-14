bdb63f2da88497ba3f641f4e2b261f7b93eb18f6
chrisIyer <hellohotbox420@gmail.com>
2025-03-14 18:02:44 -0400

    committing for night. In the middle of bad code. Must fix using lab_activity.

--- pythonBasics/refresh.py
+++ pythonBasics/refresh.py
@@ -25,4 +25,25 @@ class Instructor(Member):
           print(f"{skill} ssss")                   
           
 paul=Instructor("Paul", "figheting fires")  
-paul.add_skill("building decks and siding houses. I'm modest")        
+paul.add_skill("building decks and siding houses. I'm modest") 
+paul.intruduce()
+
+class Workshop:
+     def __init__(self, date, subject):
+          self.date = date
+          self.subject = subject
+          self.instructors = []
+          self.students = []
+
+     members = []
+     workshops = []
+
+     with open("members.csv", mode = "r") as file:
+          reader = csv.DictReader(file)
+          for row in reader:
+               if row["type"]== "Student":
+                    members.append(Student(row["full_name"], row["reason_or_bio"]))
+               elif row["type"] == "Instructor":
+                    members.append(Instructor(row["full_name"], row["reason_or bio"])) 
+                        
+
